87a373a433c6cf6abab18f37b98b0030d971f93d
alfredo fernandez <afernandez8781@hotmail.com>
2018-02-23 17:42:23 -0400

    Página de producto y ruta lista

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,10 +19,10 @@ export class ProductosService {
 
 		this.http.get("https://paginaweb-6afe3.firebaseio.com/productos_idx.json")
 				.subscribe( res => {
-					
-					console.log( res.json() );
+
+					// console.log( res.json() );
 					this.cargando = false;
-					this.productos = res.json();
+					this.productos = res.json();	
 
 				});
 
