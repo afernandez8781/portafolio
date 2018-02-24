fcc7ab7cdfb93a9f72d735ae4854805d1bdf04aa
alfredo fernandez <afernandez8781@hotmail.com>
2018-02-24 12:26:59 -0400

    Página de busqueda creada - no funciona

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {
 	AboutComponent,
 	PortafolioComponent,
-	ProductoComponent
+	ProductoComponent,
+	SearchComponent
 } from "./components/index.paginas";
 
 const app_routes: Routes = [
-  { path: 'home', component: PortafolioComponent },
-  { path: 'about', component: AboutComponent },
+  { path: "home", component: PortafolioComponent },
+  { path: "about", component: AboutComponent },
   { path: "producto/:id", component: ProductoComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: "buscar/:termino", component: SearchComponent },
+  { path: "**", pathMatch: "full", redirectTo: "home" }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, { useHash:true });
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -13,6 +13,12 @@ export class ProductosService {
 	this.cargar_productos();
 
   }
+	
+  public cargar_producto( cod:string ){
+
+	  return this.http.get(`https://paginaweb-6afe3.firebaseio.com/productos/${ cod }.json`);
+
+  }
 
   public cargar_productos(){
 	  this.cargando = true;
