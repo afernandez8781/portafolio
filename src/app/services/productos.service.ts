592956fd432c73dd3e9bb308b1d7974d16580080
alfredo fernandez <afernandez8781@hotmail.com>
2018-02-24 15:39:14 -0400

    Primera versión

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -6,12 +6,48 @@ import { Http } from '@angular/http';
 export class ProductosService {
 	
 	productos:any[] = [];
+	productos_filtrado:any[] = [];
 	cargando:boolean = true;
 
   constructor(private http:Http ) {
 	  
 	this.cargar_productos();
 
+  }
+
+  public buscar_producto( termino:string ){
+
+	// console.log("Buscando productos");
+	// console.log( this.productos.length );
+	  
+	if( this.productos.length === 0 ){
+		this.cargar_productos().then(()=>{
+			//termino la carga
+			this.filtrar_productos(termino);
+		});
+	}else{
+		this.filtrar_productos(termino);
+	}
+
+  }
+
+  private filtrar_productos(termino:string){
+
+	this.productos_filtrado = [];
+
+	termino = termino.toLowerCase();
+
+	this.productos.forEach( prod =>{
+		
+		if (prod.categoria.indexOf( termino ) >= 0 && prod.titulo.toLowerCase().indexOf( termino )){
+
+			this.productos_filtrado.push( prod );
+			// console.log("falluti", prod );
+		} 
+		
+		// console.log(prod);
+
+	})
   }
 	
   public cargar_producto( cod:string ){
@@ -23,14 +59,21 @@ export class ProductosService {
   public cargar_productos(){
 	  this.cargando = true;
 
-		this.http.get("https://paginaweb-6afe3.firebaseio.com/productos_idx.json")
-				.subscribe( res => {
+	  let promesa = new Promise( (resolve, reject )=>{
+		  
+		  this.http.get("https://paginaweb-6afe3.firebaseio.com/productos_idx.json")
+			.subscribe( res => {
 
-					// console.log( res.json() );
-					this.cargando = false;
-					this.productos = res.json();	
+				// console.log( res.json() );
+				this.cargando = false;
+				this.productos = res.json();	
+				resolve();
 
-				});
+			});
+
+	  });
+	
+	  return promesa;
 
   }
 
